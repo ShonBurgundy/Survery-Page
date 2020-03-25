20dfb510f9d11effd3b8eec41913e57f05dab0b8
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-25 15:11:22 -0700

    Explore potenial solutions for UI/Backend re: forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
   $("form#formOne").submit(function(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
     var sandwich = $("#sandwichInput").val();
     console.log(sandwich);
     
-
     $(".sandwichOutput").append(sandwich);
+
     $("#output").show();
+    event.preventDefault();
   })
 })
 
-var flavor = $("input:radio[name=flavor]:checked").val();
+var tunes = $("input:radio[name=Music]:checked").val(); 
+
+console.log(tunes);
+
+
+$(document).ready(function(){
+  $("form#formFullName").submit(function(event){
+  event.preventDefault();
+  var userFirstName = $("#fname").val();
+  var userLastName = $("#lname").val();
+
+  $("")
+  })
+})
