2130e094b3640d7153ef92986c24a913bfc74061
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-25 15:45:31 -0700

    Add

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,35 @@
+//Back-End
+
+var add = function(userFirstName, userLastName) {
+  return userFirstName + userLastName;
+};
+
+
+
 $(document).ready(function(){
-  $("form#formOne").submit(function(event) {
+  $("form#formFullName").submit(function(event) {
+    event.preventDefault();
+    var userFirstName = $("#fname").val();
+    var userLastName = $("#lname").val();
+    var result = add(userFirstName + " " , userLastName);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
     // event.preventDefault();
 
     var sandwich = $("#sandwichInput").val();
     console.log(sandwich);
     
-    $(".sandwichOutput").append(sandwich);
+    $(".sandwichOutput").text(sandwich);
 
     $("#output").show();
     event.preventDefault();
-  })
-})
+  });
+});
 
 var tunes = $("input:radio[name=Music]:checked").val(); 
 
 console.log(tunes);
-
-
-$(document).ready(function(){
-  $("form#formFullName").submit(function(event){
-  event.preventDefault();
-  var userFirstName = $("#fname").val();
-  var userLastName = $("#lname").val();
-
-  $("")
-  })
-})
